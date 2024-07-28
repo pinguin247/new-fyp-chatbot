f2ecd475e23d2dd4aa354bc291840622b50c2379
Gao Yiping <B200072@e.ntu.edu.sg>
2024-07-28 22:12:14 +0800

    Login and Register Page

---
+++ new-fyp-chatbot/app/auth/sign-in/index.tsx
@@ -0,0 +1,200 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, Link } from 'expo-router';
+import { Colors } from '@/constants/Colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function SignIn() {
+  const navigation = useNavigation();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Icon name="arrow-back" size={24} color={Colors.NAVY} />
+      </TouchableOpacity>
+      <View style={styles.subcontainer}>
+        <Text style={styles.title}>Welcome Back!</Text>
+        <Text style={styles.subtitle}>Please enter your account here</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="mail-outline" size={20} color={Colors.GRAY} style={styles.inputIcon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor={Colors.GRAY}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="lock-closed-outline" size={20} color={Colors.GRAY} style={styles.inputIcon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor={Colors.GRAY}
+          secureTextEntry={!passwordVisible}
+        />
+        <TouchableOpacity onPress={togglePasswordVisibility} style={styles.icon}>
+          <Icon name={passwordVisible ? "eye-off" : "eye"} size={20} color={Colors.GRAY} />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separatorContainer}>
+        <View style={styles.separatorLine} />
+        <Text style={styles.separatorText}>Or continue with</Text>
+        <View style={styles.separatorLine} />
+      </View>
+
+      <TouchableOpacity style={styles.googleButton}>
+        <Image
+          source={require('../../../assets/images/google.png')}
+          style={styles.googleLogo}
+        />
+        <Text style={styles.googleButtonText}>Google</Text>
+      </TouchableOpacity>
+
+      <View style={styles.registerContainer}>
+        <Text style={styles.registerText}>Don't have an account? </Text>
+        <Link href="auth/sign-up" style={styles.registerLink}>
+          Register
+        </Link>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 25,
+    backgroundColor: Colors.WHITE,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 70, // Adjust based on your app's header height
+    left: 20,
+  },
+  subcontainer: {
+    marginTop: '40%',
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: 'Outfit-Bold',
+    textAlign: 'center',
+    color: Colors.PRIMARY,
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: 'Outfit',
+    color: Colors.GRAY,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Colors.LIGHTGRAY,
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 15,
+    color: Colors.PRIMARY,
+    fontFamily: 'Outfit',
+    marginLeft: 10,
+  },
+  inputIcon: {
+    marginRight: 10,
+  },
+  icon: {
+    marginLeft: 10,
+  },
+  loginButton: {
+    width: '100%',
+    borderRadius: 10,
+    backgroundColor: Colors.NAVY,
+    padding: 15,
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    fontFamily: 'Outfit',
+    color: Colors.WHITE,
+  },
+  separatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+    width: '100%',
+  },
+  separatorLine: {
+    flex: 1,
+    height: 1,
+    backgroundColor: Colors.GRAY,
+  },
+  separatorText: {
+    fontSize: 14,
+    fontFamily: 'Outfit',
+    color: Colors.GRAY,
+    marginHorizontal: 10,
+  },
+  googleButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    borderRadius: 10,
+    backgroundColor: Colors.WHITE,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    marginTop: 20,
+  },
+  googleLogo: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    resizeMode: 'contain',
+  },
+  googleButtonText: {
+    fontFamily: 'Outfit',
+    color: Colors.BLACK,
+    fontSize: 16,
+  },
+  registerContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  registerText: {
+    fontSize: 14,
+    fontFamily: 'Outfit',
+    color: Colors.GRAY,
+  },
+  registerLink: {
+    fontSize: 14,
+    fontFamily: 'Outfit',
+    color: Colors.NAVY,
+    textDecorationLine: 'underline',
+  },
+});
---
+++ new-fyp-chatbot/components/Login.tsx
@@ -0,0 +1,76 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Colors } from '@/constants/Colors';
+import { useRouter } from 'expo-router';
+
+export default function Login() {
+  const router = useRouter();
+  return (
+    <View>
+      <Image
+        source={require('./../assets/images/TTSH.jpg')}
+        style={{
+          width: '100%',
+          height: 520,
+        }}
+      />
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: 'Outfit-Bold',
+            textAlign: 'center',
+            marginTop: 10,
+          }}
+        >
+          FitTrackSG
+        </Text>
+        <Text
+          style={{
+            fontSize: 17,
+            fontFamily: 'Outfit',
+            textAlign: 'center',
+            color: Colors.GRAY,
+            marginTop: 20,
+          }}
+        >
+          Stay active, stay healthy, and achieve your fitness goals with our
+          interactive chatbot and exercise tracking features.
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => router.push('auth/sign-in')}
+          style={styles.button}
+        >
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: 'center',
+              fontFamily: 'Outfit',
+              fontSize: 17,
+            }}
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: '100%',
+    padding: 25,
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.NAVY,
+    borderRadius: 99,
+    marginTop: '25%',
+  },
+});
