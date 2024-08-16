d3e85bcb9938eb67847e5c731f19636f273bbe8c
Gao Yiping <B200072@e.ntu.edu.sg>
2024-08-16 23:21:37 +0800

    FYP-23 Completed data fetching

--- new-fyp-chatbot-nest/src/main.ts
+++ new-fyp-chatbot-nest/src/main.ts
@@ -1,11 +1,13 @@
 import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors()); // Enable CORS
   const port = process.env.PORT || 3000 || 3001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
