de351cec81b52be772d78513826e666ab5583bcf
Gao Yiping <B200072@e.ntu.edu.sg>
2024-08-20 14:57:33 +0800

    FYP-26 Completed Supabase Auth in Next.js

--- patient-tracker/app/page.tsx
+++ patient-tracker/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <h1>Data from Sup</h1>
+      <h1>Data from Supabase</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
