0bc59aceefe0de6b269f8b91e742fdf80cbafcfe
Gao Yiping <B200072@e.ntu.edu.sg>
2024-08-22 01:12:04 +0800

    Save chatbot history

---
+++ new-fyp-chatbot/lib/saveMessage.ts
@@ -0,0 +1,28 @@
+export const saveMessage = async (
+  userId: string,
+  content: string,
+  role: string,
+) => {
+  console.log('Saving bot message for userId:', userId);
+  try {
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_MY_ENDPOINT}/saveMessage`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          userId: userId,
+          content: content,
+          role: role,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Failed to save message');
+    }
+  } catch (error) {
+    console.error('Error saving message:', error);
+  }
+};
